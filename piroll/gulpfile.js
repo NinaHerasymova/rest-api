fcd31d5dbb1e47442dc39e9927963111b2ec6d30
Anton Diachenko <dao9714@gmail.com>
2020-03-14 18:00:17 +0200

    add new structure

--- piroll/gulpfile.js
+++ piroll/gulpfile.js
@@ -1,13 +1,17 @@
 const gulp = require('gulp')
 const sass = require('gulp-sass')
+const sassImport = require('gulp-sass-import')
 const rename = require('gulp-rename')
 const browserSync = require('browser-sync').create()
 
 function style() {
-    return gulp.src('./scss/**/*.scss')
-            .pipe(sass())
+    return gulp.src('./src/scss/main.scss')
+            .pipe(sassImport({
+                filename: 'main'
+            }))
+            .pipe(sass().on('error', sass.logError))
             .pipe(rename({basename: 'style'}))
-            .pipe(gulp.dest('./css'))
+            .pipe(gulp.dest('./src/css'))
             .pipe(browserSync.stream())
 }
 
@@ -17,7 +21,7 @@ function watch() {
             baseDir: './'
         }
     })
-    gulp.watch('./scss/**/*.scss', style)
+    gulp.watch('./src/scss/**/*.scss', style)
     gulp.watch('./*.html').on('change', browserSync.reload)
 }
 
